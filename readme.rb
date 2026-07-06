35dbf327affc7e969cf1ab9f2ddeeb612f1c6520
Vincent <funnymoose234@gmail.com>
2026-07-05 23:33:29 -0700

    readme.rb

--- readme.rb
+++ readme.rb
@@ -1,5 +1,33 @@
 readme
-install brew
-install VScode
-install git
+  # --- Core Tools ---
+  install brew
+  install git
+  install VSCode
+
+  # --- Python Environment ---
+  install pyenv
+  install python 3.12
+  install pip
+  install virtualenv
+
+  # --- Data Science Libraries ---
+  install numpy
+  install pandas
+  install matplotlib
+  install seaborn
+  install scikit-learn
+  install jupyter
+
+  # --- Optional: Deep Learning ---
+  install pytorch
+  install tensorflow
+
+  # --- Database & Storage ---
+  install postgresql
+  install duckdb
+
+  # --- VSCode Extensions ---
+  install extension python
+  install extension jupyter
+  install extension ruff
 end
